7c5d88ebe2d00683790386f018b78bfe3035334e
fernando-grande <fsg0314@gmail.com>
2024-04-25 14:19:36 -0300

    font

--- frontend/src/screens/Experiments/printForm.tsx
+++ frontend/src/screens/Experiments/printForm.tsx
@@ -7,6 +7,7 @@ export function PrintForm() {
 
     return (
         <div className="flex justify-center items-center align-middle">
+            <p className="pb-4 pt-2 text-blue-600 text-2xl">Experiment METADATA </p>
             <pre>{JSON.stringify(values.email, null, 2)}</pre>
             <pre>{JSON.stringify(values, null, 2)}</pre>
         </div>
