5a3e1a8d2d59a409f4e4d3d72af4271b9b5e481b
fernando-grande <fsg0314@gmail.com>
2024-06-13 23:18:00 -0300

    fix documentation

--- frontend/src/forms/experiment/Documentation.tsx
+++ frontend/src/forms/experiment/Documentation.tsx
@@ -28,27 +28,59 @@ export function Documentation() {
 
             <label htmlFor="documentation-abstract-abstractBackground">Abstract backgound: </label>
             <input id="documentation-abstract-abstractBackground" className="w-96 border-[1px] p-2 rounded-md mb-6" {...register('documentation.abstract.abstractBackground')} />
-            {errors.documentation?.template && <span>{errors.documentation.template.message}</span>}
+            {errors.documentation?.abstract?.abstractBackground && <span>{errors.documentation.abstract.abstractBackground.message}</span>}
 
             <label htmlFor="documentation-abstract-methods">Methods: </label>
             <input id="documentation-abstract-methods" className="w-96 border-[1px] p-2 rounded-md mb-6" {...register('documentation.abstract.methods')} />
-            {errors.documentation?.template && <span>{errors.documentation.template.message}</span>}
+            {errors.documentation?.abstract?.methods && <span>{errors.documentation.abstract.methods.message}</span>}
 
             <label htmlFor="documentation-abstract-results">Results: </label>
             <input id="documentation.abstract.results" className="w-96 border-[1px] p-2 rounded-md mb-6" {...register('documentation.abstract.results')} />
-            {errors.documentation?.template && <span>{errors.documentation.template.message}</span>}
+            {errors.documentation?.abstract?.results && <span>{errors.documentation.abstract.results.message}</span>}
 
             <label htmlFor="documentation-abstract-limitations">Limitations: </label>
             <input id="documentation-abstract-limitations" className="w-96 border-[1px] p-2 rounded-md mb-6" {...register('documentation.abstract.limitations')} />
-            {errors.documentation?.template && <span>{errors.documentation.template.message}</span>}
+            {errors.documentation?.abstract?.limitations && <span>{errors.documentation.abstract.limitations.message}</span>}
 
             <label htmlFor="documentation-abstract-conclusions">Conclusions: </label>
             <input id="documentation-abstract-conclusions" className="w-96 border-[1px] p-2 rounded-md mb-6" {...register('documentation.abstract.conclusions')} />
-            {errors.documentation?.template && <span>{errors.documentation.template.message}</span>}
+            {errors.documentation?.abstract?.conclusions && <span>{errors.documentation.abstract.conclusions.message}</span>}
 
             <label htmlFor="documentation-abstract-keywords">Keywords: </label>
             <input id="documentation-abstract-keywords" className="w-96 border-[1px] p-2 rounded-md mb-6" {...register('documentation.abstract.keywords')} />
-            {errors.documentation?.template && <span>{errors.documentation.template.message}</span>}
+            {errors.documentation?.abstract?.keywords && <span>{errors.documentation.abstract.keywords.message}</span>}
+
+            <p className="text-xl font-semibold pb-2">INTRODUCTION</p>
+
+            <label htmlFor="documentation-introduction-problemStatement">Problem Statement: </label>
+            <input id="documentation-introduction-problemStatement" className="w-96 border-[1px] p-2 rounded-md mb-6" {...register('documentation.introduction.problemStatement')} />
+            {errors.documentation?.introduction?.problemStatement && <span>{errors.documentation.introduction.problemStatement.message}</span>}
+
+            <label htmlFor="documentation-introduction-researchObjectives">Research Objectives: </label>
+            <input id="documentation-introduction-researchObjectives" className="w-96 border-[1px] p-2 rounded-md mb-6" {...register('documentation.introduction.researchObjectives')} />
+            {errors.documentation?.introduction?.researchObjectives && <span>{errors.documentation.introduction.researchObjectives.message}</span>}
+
+            <label htmlFor="documentation-introduction-context">Context: </label>
+            <input id="documentation-introduction-context" className="w-96 border-[1px] p-2 rounded-md mb-6" {...register('documentation.introduction.context')} />
+            {errors.documentation?.introduction?.context && <span>{errors.documentation.introduction.context.message}</span>}
+
+            <p className="text-xl font-semibold pb-2">RELATED WORK</p>
+
+            <label htmlFor="documentation-relatedWork-technologyUnderInvestigation">Technology Under Investigation: </label>
+            <input id="documentation-relatedWork-technologyUnderInvestigation" className="w-96 border-[1px] p-2 rounded-md mb-6" {...register('documentation.relatedWork.technologyUnderInvestigation')} />
+            {errors.documentation?.relatedWork?.technologyUnderInvestigation && <span>{errors.documentation.relatedWork.technologyUnderInvestigation.message}</span>}
+
+            <label htmlFor="documentation-relatedWork-alternativeTechnologies">Alternative Technologies: </label>
+            <input id="documentation-relatedWork-alternativeTechnologies" className="w-96 border-[1px] p-2 rounded-md mb-6" {...register('documentation.relatedWork.alternativeTechnologies')} />
+            {errors.documentation?.relatedWork?.alternativeTechnologies && <span>{errors.documentation.relatedWork.alternativeTechnologies.message}</span>}
+
+            <label htmlFor="documentation-relatedWork-relatedStudiest">Related Studies: </label>
+            <input id="documentation-relatedWork-relatedStudies" className="w-96 border-[1px] p-2 rounded-md mb-6" {...register('documentation.relatedWork.relatedStudies')} />
+            {errors.documentation?.relatedWork?.relatedStudies && <span>{errors.documentation.relatedWork.relatedStudies.message}</span>}
+
+            <label htmlFor="documentation-relatedWork-relevancePractice">Relevance Practice: </label>
+            <input id="documentation-relatedWork-relevancePractice" className="w-96 border-[1px] p-2 rounded-md mb-6" {...register('documentation.relatedWork.relevancePractice')} />
+            {errors.documentation?.relatedWork?.relevancePractice && <span>{errors.documentation.relatedWork.relevancePractice.message}</span>}
 
             <button type="submit" className="font-bold text-white mb-6 border-[1px] p-2 rounded-md bg-sky-700 w-24">
                 NEXT
